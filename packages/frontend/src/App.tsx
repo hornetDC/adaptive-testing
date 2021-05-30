a962bd8a30022cc45daed1c20b04d9bbf7bff430
= <=>
2021-05-30 15:13:04 +0300

    Changed router

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import AuthContext from 'context/AuthContext';
 import Login from 'views/Login';
